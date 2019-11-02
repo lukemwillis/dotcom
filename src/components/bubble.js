dbd7de0b38cd553cef001b91d0e30c6d0c08a484
Luke Willis <luke.willis@parivedasolutions.com>
2019-11-01 22:12:42 -0700

    updating styles & adding pages

--- src/components/bubble.js
+++ src/components/bubble.js
@@ -1,46 +1,43 @@
 import React from "react"
-import { Link } from "gatsby"
 import { motion } from "framer-motion"
 
-const Bubble = ({ right = false, link = null, children }) => {
-  const foreground = link ? "#fff" : "#000"
-  const background = link ? "#555" : "#eee"
-  const shadow = link ? "#eee" : "#555"
+const Bubble = ({ children }) => {
+  const foreground = "#000"
+  const background = "#eee"
+  const shadow = "#555"
   return (
     <div
       style={{
         display: "flex",
         flexDirection: "row",
-        justifyContent: right ? "flex-end" : "flex-start",
+        justifyContent: "flex-start",
+        marginRight: "1em",
       }}
     >
       <motion.div
-        initial={{ opacity: 0, y: 100 }}
+        initial={{ opacity: 0, y: "10em" }}
         animate={{
           opacity: 1,
           y: 0,
         }}
         exit={{
           opacity: 0,
-          y: -100,
+          y: "-10em",
+          transition: {
+            type: "tween",
+          },
         }}
         style={{
           position: "relative",
           background: background,
           color: foreground,
-          borderRadius: `1em 1em ${right ? "0 1em" : "1em 0"}`,
+          borderRadius: `1em 1em 1em 0`,
           padding: `.5em`,
           marginBottom: "1em",
           boxShadow: `0 .25em 0 0 ${shadow}`,
         }}
       >
-        {link ? (
-          <Link style={{ color: foreground }} to={link}>
-            {children}
-          </Link>
-        ) : (
-          children
-        )}
+        {children}
         <div
           style={{
             position: "absolute",
@@ -48,10 +45,8 @@ const Bubble = ({ right = false, link = null, children }) => {
             width: 0,
             borderWidth: ".25em",
             borderStyle: "solid",
-            borderColor: right
-              ? `${background} ${background} transparent transparent`
-              : `${background} transparent transparent ${background}`,
-            ...(right ? { right: 0 } : { left: 0 }),
+            borderColor: `${background} transparent transparent ${background}`,
+            left: 0,
           }}
         />
       </motion.div>
